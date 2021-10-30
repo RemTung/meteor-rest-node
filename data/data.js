8a4e43c126a791c7a84aeb2cbbfd73efef878d12
remustung <remustung@gmail.com>
2021-10-30 02:30:30 -0400

    .

--- data/data.js
+++ data/data.js
@@ -2,51 +2,147 @@ module.exports = {
     meteor: [
         {
             id: '1',
-            name: 'Orionids'
+            name: 'Orionids',
+            status: 'Active from September 26th to November 22nd, 2021',
+      //activeYN: 0 is unactive 1 is active
+            activeYN: 1,
+            description: 'The Orionids are a medium strength shower that sometimes reaches high strength activity. In a normal year the Orionids produce 10-20 shower members at maximum. In exceptional years, such as 2006-2009, the peak rates were on par with the Perseids (50-75 per hour). Recent displays have produced low to average displays of this shower.',
+            velocity: '41 miles/sec (swift - 66km/sec)',
+      //speedQual: 1 is slow, 2 is medium, 3 is swift
+            speedQual: 3,
+            veloNum: 41
         },
         {
             id: '2',
-            name: 'Southern Taurids'
+            name: 'Southern Taurids',
+            status: 'Active from September 28th to December 2nd, 2021',
+      //activeYN: 0 is unactive 1 is active
+            activeYN: 1,
+            description: 'The Southern Taurids are a long-lasting shower that several peaks during its activity period. The shower is active for more than two months but rarely produces more than five shower members per hour, even at maximum activity. The Taurids (both branches) are rich in fireballs and are often responsible for increased number of fireball reports from September through November.',
+            velocity: '17.2 miles/sec (slow - 27.7km/sec)',
+      //speedQual: 1 is slow, 2 is medium, 3 is swift
+            speedQual: 1,
+            veloNum: 17.2
         },
         {
             id: '3',
-            name: 'Northern Taurids'
+            name: 'Northern Taurids',
+            status: 'Active from October 13th to December 2nd, 2021',
+      //activeYN: 0 is unactive 1 is active
+            activeYN: 1,
+            description: 'This shower is much like the Southern Taurids, just active a bit later in the year. When the two showers are active simultaneously in late October and early November, there is sometimes a notable increase in the fireball activity. There seems to be a seven year periodicity with these fireballs. 2008 and 2015 both produced remarkable fireball activity. 2022 may be the next opportunity.',
+            velocity: '18 miles/sec (slow - 30km/sec)',
+      //speedQual: 1 is slow, 2 is medium, 3 is swift
+            speedQual: 1,
+            veloNum: 18
         },
         {
             id: '4',
-            name: 'Leonids'
+            name: 'Leonids',
+            status: 'Active from November 3rd, 2021 to December 2nd, 2021',
+      //activeYN: 0 is unactive 1 is active
+            activeYN: 1,
+            description: 'The Leonids are best known for producing meteor storms in the years of 1833, 1866, 1966, 1999, and 2001. These outbursts of meteor activity are best seen when the parent object, comet 55P/Tempel-Tuttle, is near perihelion (closest approach to the sun). Yet it is not the fresh material we see from the comet, but rather debris from earlier returns that also happen to be most dense at the same time. Unfortunately, it appears that the earth will not encounter any dense clouds of debris until 2099. Therefore, when the comet returns in 2031 and 2064, there will be no meteor storms, but perhaps several good displays of Leonid activity when rates are in excess of 100 per hour. The best we can hope for now until the year 2030 is peaks of around 15 shower members per hour and perhaps an occasional weak outburst when the earth passes near a debris trail. The Leonids are often bright meteors with a high percentage of persistent trains.',
+            velocity: '43.5 miles/sec (swift - 70km/sec)',
+      //speedQual: 1 is slow, 2 is medium, 3 is swift
+            speedQual: 3,
+            veloNum: 43.5
         },
         {
             id: '5',
-            name: 'Geminids'
+            name: 'Geminids',
+            status: 'Active from November 19th, 2021 to December 24th, 2021',
+      //activeYN: 0 is unactive 1 is active
+            activeYN: 1,
+            description: 'The Geminids are usually the strongest meteor shower of the year and meteor enthusiasts are certain to circle December 13 and 14 on their calendars. This is the one major shower that provides good activity prior to midnight as the constellation of Gemini is well placed from 22:00 onward. The Geminids are often bright and intensely colored. Due to their medium-slow velocity, persistent trains are not usually seen. These meteors are also seen in the southern hemisphere, but only during the middle of the night and at a reduced rate.',
+            velocity: '21 miles/sec (medium - 34km/sec)',
+      //speedQual: 1 is slow, 2 is medium, 3 is swift
+            speedQual: 2,
+            veloNum: 21
         },
         {
             id: '6',
-            name: 'Ursids'
+            name: 'Ursids',
+            status: 'Active from December 13th, 2021 to December 24th, 2021',
+      //activeYN: 0 is unactive 1 is active
+            activeYN: 1,
+            description: 'The Ursids are often neglected due to the fact it peaks just before Christmas and the rates are much less than the Geminds, which peaks just a week before the Ursids. Observers will normally see 5-10 Ursids per hour during the late morning hours on the date of maximum activity. There have been occasional outbursts when rates have exceeded 25 per hour. These outbursts appear unrelated to the perihelion dates of comet 8P/Tuttle. This shower is strictly a northern hemisphere event as the radiant fails to clear the horizon or does so simultaneously with the start of morning twilight as seen from the southern tropics.',
+            velocity: '20.5 miles/sec (medium - 33km/sec)',
+      //speedQual: 1 is slow, 2 is medium, 3 is swift
+            speedQual: 2,
+            veloNum: 20.5
         },
         {
             id: '7',
-            name: 'Quadrantids'
+            name: 'Quadrantids',
+            status: 'Active from December 26th, 2021 to January 16th, 2022',
+      //activeYN: 0 is unactive 1 is active
+            activeYN: 1,
+            description: 'The Quadrantids have the potential to be the strongest shower of the year but usually fall short due to the short length of maximum activity (6 hours) and the poor weather experienced during early January. The average hourly rates one can expect under dark skies is 25. These meteors usually lack persistent trains but often produce bright fireballs. Due to the high northerly declination (celestial latitude) these meteors are not well seen from the southern hemisphere.',
+            velocity: '25 miles/sec (medium - 40.2km/sec)',
+      //speedQual: 1 is slow, 2 is medium, 3 is swift
+            speedQual: 2,
+            veloNum: 25
         },
         {
             id: '8',
-            name: 'Lyrids'
+            name: 'Lyrids',
+            status: 'Active from April 15th, 2022 to April 29th, 2022',
+      //activeYN: 0 is unactive 1 is active
+            activeYN: 1,
+            description: 'The Lyrids are a medium strength shower that usually produces good rates for three nights centered on the maximum. These meteors also usually lack persistent trains but can produce fireballs. These meteors are best seen from the northern hemisphere where the radiant is high in the sky at dawn. Activity from this shower can be seen from the southern hemisphere, but at a lower rate.',
+            velocity: '30 miles/sec (medium - 48.4km/sec)',
+      //speedQual: 1 is slow, 2 is medium, 3 is swift
+            speedQual: 2,
+            veloNum: 30
         },
         {
             id: '9',
-            name: 'eta Aquariids'
+            name: 'eta Aquariids',
+            status: 'Active from April 15th, 2022 to May 27th, 2022',
+      //activeYN: 0 is unactive 1 is active
+            activeYN: 1,
+            description: 'The Eta Aquariids are a strong shower when viewed from the southern tropics. From the equator northward, they usually only produce medium rates of 10-30 per hour just before dawn. Activity is good for a week centered the night of maximum activity. These are swift meteors that produce a high percentage of persistent trains, but few fireballs.',
+            velocity: '40.7 miles/sec (swift - 65.5km/sec)',
+      //speedQual: 1 is slow, 2 is medium, 3 is swift
+            speedQual: 3,
+            veloNum: 40.7
         },
         {
             id: '10',
-            name: 'Southern delta Aquariids'
+            name: 'Southern delta Aquariids',
+            status: 'Active from July 18th, 2022 to August 21st, 2022',
+      //activeYN: 0 is unactive 1 is active
+            activeYN: 1,
+            description: 'The Delta Aquariids are another strong shower best seen from the southern tropics. North of the equator the radiant is located lower in the southern sky and therefore rates are less than seen from further south. These meteors produce good rates for a week centered on the night of maximum. These are usually faint meteors that lack both persistent trains and fireballs.',
+            velocity: '25 miles/sec (medium - 40km/sec)',
+      //speedQual: 1 is slow, 2 is medium, 3 is swift
+            speedQual: 2,
+            veloNum: 25
         },
         {
             id: '11',
-            name: 'alpha Capricornids'
+            name: 'alpha Capricornids',
+            status: 'Active from July 7th, 2022 to August 15th, 2022',
+      //activeYN: 0 is unactive 1 is active
+            activeYN: 1,
+            description: 'The Alpha Capricornids are active from July 7 through August 15 with a plateau-like maximum centered on July 31st. This shower is not very strong and rarely produces in excess of five shower members per hour. What is notable about this shower is the number of bright fireballs produced during its activity period. This shower is seen equally well on either side of the equator.',
+            velocity: '14 miles/sec (slow - 22km/sec)',
+      //speedQual: 1 is slow, 2 is medium, 3 is swift
+            speedQual: 1,
+            veloNum: 14
         },
         {
             id: '12',
-            name: 'Perseids'
+            name: 'Perseids',
+            status: 'Active from July 14th, 2022 to September 1st, 2022',
+      //activeYN: 0 is unactive 1 is active
+            activeYN: 1,
+            description: 'The Perseids are the most popular meteor shower as they peak on warm August nights as seen from the northern hemisphere. The Perseids are active from July 14 to September 1. They reach a strong maximum on August 12 or 13, depending on the year. Normal rates seen from rural locations range from 50-75 shower members per hour at maximum. The Perseids are particles released from comet 109P/Swift-Tuttle during its numerous returns to the inner solar system. They are called Perseids since the radiant (the area of the sky where the meteors seem to originate) is located near the prominent constellation of Perseus the hero when at maximum activity.',
+            velocity: '37 miles/sec (swift - 59km/sec)',
+      //speedQual: 1 is slow, 2 is medium, 3 is swift
+            speedQual: 3,
+            veloNum: 37
         }
     ]
 };
